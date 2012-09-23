9d0478919d6d95f581f47bbcf52e1462c802de36
nevercoded <nevercoded@outlook.com>
2012-09-23 16:11:04 -0700

    Algos1 project. Added Stack implementation

---
+++ Algos1/src/ds/IsuStringStack.java
@@ -0,0 +1,12 @@
+package ds;
+
+/**
+ * Interface for stack FILO 
+ * @author nevercoded
+ */
+public interface IsuStringStack {
+	
+	public void push(String item);
+	public String pop();
+	public boolean isEmpty();	
+}
